863614bc1eb61d6ab3c75f7bff739dbc330d10f3
Volam1207 <nguyenthevolam127@gmail.com>
2024-03-19 17:24:26 +0700

    tkinter

--- buoi 20_1/hienthitenuser.py
+++ buoi 20_1/hienthitenuser.py
@@ -9,14 +9,17 @@ def hovaten(): #đătt tên trong str phải trùng tên hàm
     num2 = str(ten.get())
     result = num1 + " " + num2
     kq.config(text="Xin chao" + " " + str(result))
+    
 Label(root, text = "Họ").grid(row = 0, column = 0)
 #capnhat o input1
 ho = Entry(root)
 ho.grid(row = 0, column = 1)
+
 Label(root, text = "Tên").grid(row = 1, column = 0)
 #capnhat o input 2
 ten = Entry(root)
 ten.grid(row = 1, column = 1)
+
 Button(root, text = "Xử Lý ",command = hovaten).grid(row = 2, column = 0)
 #capnhat label kq
 kq=Label(root,text="Xin chao")
--- tkinter/weight_convertion.py
+++ tkinter/weight_convertion.py
@@ -4,38 +4,38 @@ root = Tk()
 root.geometry("500x600")
 root.title("Weight Convert")
 
-def weight_convert():
-    kg_value = e2_value.get()  # Get the value from the Entry widget
+def weight_cover():
+    kg = e2_value.get() #lấy giá trị từ e2
 
-    gram = float(kg_value) * 1000
+    gram = float(kg) * 1000
     gram_entry.delete(1.0, END)
     gram_entry.insert(END, gram)
 
-    pounds = float(kg_value) * 2.20462
+    pounds = float(kg) * 2.20462
     pound_entry.delete(1.0, END)
     pound_entry.insert(END, pounds)
 
-    ounce = float(kg_value) * 35.274
+    ounce = float(kg) * 35.274
     ounce_entry.delete(1.0, END)
     ounce_entry.insert(END, ounce)
-
-# Khởi tạo GUI
+   
+#Khởi tạo GUI
 e1 = Label(root, text="Nhập giá trị kg:")  # Sử dụng window làm parent
 e2_value = StringVar()
 e2 = Entry(root, textvariable=e2_value)
-b1 = Button(root, text="Chuyển đổi", command=weight_convert)
+b1 = Button(root, text="Chuyển đổi", command=weight_cover)
 e3 = Label(root, text="Gram:")
 e4 = Label(root, text="Pound:")
-e5 = Label(root, text="Ounce:")
+e5 =Label(root, text="Ounce:")
 
-# Sắp xếp grid
+#sắp xếp grid
 e1.grid(row=0, column=0)
 e2.grid(row=0, column=1)
 b1.grid(row=0, column=2)
 e3.grid(row=1, column=0)
 e4.grid(row=2, column=0)
 e5.grid(row=3, column=0)
-#hien thi duoi dang text
+#luu gia trị 
 gram_entry = Text(height=1, width=20)
 pound_entry = Text(height=1, width=20)
 ounce_entry = Text(height=1, width=20)
